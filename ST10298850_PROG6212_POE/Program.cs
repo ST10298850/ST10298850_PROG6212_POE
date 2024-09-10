dade43a3e98ad893f240762754faad3bffa5451a
Luke Petzer <st10298850@vcconnect.edu.za>
2024-09-10 18:38:51 +0200

    Referencing added.

--- ST10298850_PROG6212_POE/Program.cs
+++ ST10298850_PROG6212_POE/Program.cs
@@ -25,3 +25,10 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+//--------------------------------------------------------REFERENCES--------------------------------------------------------
+// Microsoft. (2023). Introduction to ASP.NET Core. [online] Available at: https://learn.microsoft.com/en-us/aspnet/core/introduction-to-aspnet-core?view=aspnetcore-8.0 [Accessed 10 September 2024].
+
+//Japikse, P. & Troelsen, A. W., 2022. Pro C 10 with .NET 6:Foundational Principles and Practices in Programming. 11th ed. New York: Apress.
+
+//--------------------------------------------------------END OF FILE--------------------------------------------------------
